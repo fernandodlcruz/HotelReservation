5b56fb22f0abb66f521f40a793155561d337b87c
fernandodlcruz <fernando.dlcruz@gmail.com>
2019-02-09 11:41:06 -0800

    Created RoomDAO calss

---
+++ src/com/hotelreservation/repository/RoomDAO.java
@@ -0,0 +1,12 @@
+package com.hotelreservation.repository;
+
+import java.util.List;
+import com.hotelreservation.model.Customer;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RoomDAO implements IRepository<Room> {
+
+}
